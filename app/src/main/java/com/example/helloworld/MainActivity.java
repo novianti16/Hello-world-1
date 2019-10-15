faaf4ea3887e6c8d147c093f106949484e23e756
Novianti Rahayu <pastelmarun@gmail.com>
2019-10-15 11:38:31 +0700

    Skor Basket.

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int skorA = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void tambah3poinA(View view) {
+        skorA = skorA + 3;
+        TextView textViewA = findViewById(R.id.skorA);
+        textViewA.setText (String.valueOf(skorA));
     }
 }
